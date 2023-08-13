3c4341351bced6908a36c404513541fe42477fee
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-08-14 01:40:03 +0300

    The business layer has been started and a certain part has been completed.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Delete(Product product);
+        void Update(Product product);
+    }
+}
